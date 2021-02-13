c0c3f9dc44ab22704ab2520cbe35eff98c2ad3fd
emirtugayli <emirtugayli03@gmail.com>
2021-02-13 18:23:27 +0300

    update 13/2/2020 6:23 pm

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string CarAdded = "Araba başarıyla eklendi.";
+        public static string ErrorCarAdd = "Araba eklenemedi ! Lutfen gunluk fiyatinin 0'dan buyuk olduguna ve isminin min 2 karakter oldugundan emin olunuz.";
+        public static string CarDeleted = "Araba başarıyla silindi.";
+        public static string ErrorCarDelete = "Araba silinemedi ! Lutfen girilen Araba ID'sinin dogru olup olmadigindan emin olunuz.";
+        public static string CarUpdated = "Araba başarıyla guncellendi.";
+        public static string ErrorCarUpdate = "Araba guncellenemedi ! Lutfen istenen bilgilerin dogru oldugundan ya da Araba ID'sinin dogru oldugundan emin olunuz.";
+        public static string ColorAdded = "Renk başarıyla eklendi.";
+        public static string ErrorColorAdd = "Renk eklenemedi ! Lutfen istenen bilgilerin dogru formda oldugundan emin olunuz";
+        public static string ColorDeleted = "Renk başarıyla silindi.";
+        public static string ErrorColorDelete = "Renk silinemedi ! Rengin ID'sinin dogru oldugundan emin olunuz.";
+        public static string ColorUpdated = "Renk başarıyla guncellendi.";
+        public static string ErrorColorUpdate = "Renk guncellenemedi ! Lutfen istenen bilgilerin dogru oldugundan ya da Renk ID'sinin dogru oldugundan emin olunuz.";
+        public static string BrandAdded = "Marka başarıyla eklendi.";
+        public static string ErrorBrandAdd = "Marka eklenemedi ! Lutfen istenen bilgilerin dogru formda oldugundan emin olunuz";
+        public static string BrandDeleted = "Marka başarıyla silindi.";
+        public static string ErrorBrandDelete = "Marka silinemedi ! Lutfen Marka ID'sinin dogru oldugundan emin olunuz. ";
+        public static string BrandUpdated = "Marka başarıyla guncellendi.";
+        public static string ErrorBrandUpdate = "Marka guncellenemedi ! Lutfen istenen bilgilerin dogru oldugundan ya da Marka ID'sinin dogru oldugundan emin olunuz.";
+
+    }
+}
+
---
+++ Core/Utilities/ErrorResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities
+{
+    public class ErrorResult : Result
+    {
+        public ErrorResult(string message) : base(false)
+        {
+        }
+        public ErrorResult() : base(false)
+        {
+        }
+    }
+}
